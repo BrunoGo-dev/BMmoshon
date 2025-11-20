3acd3e93742b517cc724252c02be2db83ff0aabb
brayan olivas <0621enblanco@gmail.com>
2025-11-20 17:19:39 -0500

    Preparar proyecto para Render con DB remota-4

--- app/models/conexion/cls_conectar.php
+++ app/models/conexion/cls_conectar.php
@@ -3,15 +3,15 @@ class Conexion {
 
     function getConexion() {
 
-        // SIEMPRE intentar leer las variables de entorno de Docker
-        $server = getenv('sql.freedb.tech');
-        $login  = getenv('freedb_brayan0621');
-        $pass   = getenv('AU!p53czm@U?5Aj');
-        $bdatos = getenv('freedb_bmmoda1');
+        // Leer variables de entorno correctamente
+        $server = getenv('DB_HOST');
+        $login  = getenv('DB_USER');
+        $pass   = getenv('DB_PASSWORD');
+        $bdatos = getenv('DB_NAME');
 
         // Validar que existan
         if (!$server || !$login || !$bdatos) {
-            die("❌ Error: Variables de entorno DB_* no están definidas. Revisa tu docker-compose.");
+            die("❌ Error: Variables de entorno DB_* no están definidas.");
         }
 
         // Conexión
